a588e089cc74efa234fa3322516ff36d0dd65077
VladimirFabri98 <vfficial98@gmail.com>
2026-05-04 12:49:47 +0200

    Vezbe 5 - Integracioni testovi - 04.05.2026.

--- BackendProject/src/main/java/rva/models/Porudzbina.java
+++ BackendProject/src/main/java/rva/models/Porudzbina.java
@@ -12,28 +12,28 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
+
 @Entity
 public class Porudzbina {
 
 	@Id
-	@SequenceGenerator(name = "porudzbina_seq", sequenceName = "porudzbina_seq",
-	allocationSize = 1)
+	@SequenceGenerator(name = "porudzbina_seq", sequenceName = "porudzbina_seq", allocationSize = 1)
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "porudzbina_seq")
 	private long id;
 	private Date datumPorudzbine;
 	private Date datumIsporuke;
 	private double iznos;
 	private boolean placeno;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "dobavljac")
 	private Dobavljac dobavljac;
-	
+
 	@OneToMany(mappedBy = "porudzbina", cascade = CascadeType.ALL)
 	private List<StavkaPorudzbine> stavke;
-	
+
 	public Porudzbina() {
-		
+
 	}
 
 	public Porudzbina(Date datumPorudzbine, Date datumIsporuke, double iznos, boolean placeno) {
@@ -82,6 +82,14 @@ public class Porudzbina {
 
 	public void setPlaceno(boolean placeno) {
 		this.placeno = placeno;
+	}
+
+	public Dobavljac getDobavljac() {
+		return dobavljac;
+	}
+
+	public void setDobavljac(Dobavljac dobavljac) {
+		this.dobavljac = dobavljac;
 	}
 
 }
