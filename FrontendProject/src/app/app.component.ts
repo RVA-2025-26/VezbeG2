4a43545b15ed9bd56f7dba9a1662a3aca33594d5
VladimirFabri98 <vfficial98@gmail.com>
2026-05-18 12:27:54 +0200

    Vezbe 7 - App component - 18.05.2026.

--- FrontendProject/src/app/app.component.ts
+++ FrontendProject/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIcon } from '@angular/material/icon';
+import { MatNavList } from '@angular/material/list';
+import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatExpansionModule, MatIcon, MatNavList,
+    MatSidenav, MatSidenavContainer, RouterLink, CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -11,3 +19,4 @@ export class AppComponent {
   title = 'Novi naziv';
   datum = Date.now();
 }
+
